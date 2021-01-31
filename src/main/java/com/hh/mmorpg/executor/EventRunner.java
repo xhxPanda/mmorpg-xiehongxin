be1d6ac0f204f75c562eabbc8d5a75d1ef094338
XieHongXin <171510228@qq.com>
2021-01-31 14:16:15 +0800

    （线程模型）：新线程模型提交

--- src/main/java/com/hh/mmorpg/executor/EventRunner.java
+++ src/main/java/com/hh/mmorpg/executor/EventRunner.java
@@ -2,10 +2,24 @@ package com.hh.mmorpg.executor;
 
 import java.util.function.Consumer;
 
-public class EventRunner implements Runnable {
+public abstract class EventRunner implements Runnable {
+
+    private Consumer<EventRunner> preRun;
+
+    private Consumer<EventRunner> afterRun;
+
+    public abstract int code();
+
+    public abstract void runEvent();
+
+    public abstract String name();
 
     @Override
     public void run() {
+        preRun.accept(this);
 
+        runEvent();
+
+        afterRun.accept(this);
     }
 }
--- src/main/java/com/hh/mmorpg/executor/MultiThreadEventExecutorGroup.java
+++ src/main/java/com/hh/mmorpg/executor/MultiThreadEventExecutorGroup.java
@@ -1,16 +1,24 @@
 package com.hh.mmorpg.executor;
 
+import io.netty.util.concurrent.DefaultEventExecutor;
+import io.netty.util.concurrent.DefaultThreadFactory;
+
 import java.util.concurrent.ThreadFactory;
 
 public class MultiThreadEventExecutorGroup {
     private EventExecutor[] eventExecutors;
 
+    private int threadNum;
+
     public void init(int threadNum) {
         eventExecutors = new EventExecutor[threadNum];
-//        ThreadFactory threadFactory = T
+        ThreadFactory threadFactory = new DefaultThreadFactory("group_thread");
         for (int i = 0; i< threadNum; i++) {
-
+            eventExecutors[i] = new EventExecutor(new DefaultEventExecutor(), threadFactory, true);
         }
     }
 
+    public void pushRunner(int code, Runnable runnable) {
+        eventExecutors[code % threadNum].execute(runnable);
+    }
 }
